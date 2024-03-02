2e76a634e25f5a518f29a4c13e411d91eae58747
Fruttolo <s.contino97@gmail.com>
2024-03-02 20:37:52 +0100

    second commit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from binance.client import Client
 from binance.enums import *
 from dotenv import load_dotenv
 import os
+from time import sleep
 
 # Constants
 # Load the environment variables from .env file
@@ -11,6 +12,9 @@ load_dotenv()
 API_KEY = os.getenv("API_KEY")
 API_SECRET = os.getenv("API_SECRET")
 PAIR = os.getenv("PAIR")
+QUANTITY = os.getenv("QUANTITY")
+TAKEPROFIT = float(os.getenv("TAKEPROFIT"))
+STOPLOSS = float(os.getenv("STOPLOSS"))
 
 # Initialize the Binance client
 client = Client(API_KEY, API_SECRET)
@@ -19,95 +23,66 @@ def getPrice():
     price = client.get_symbol_ticker(symbol=PAIR)
     return price['price']
 
-def buyMarket(quantity):
-    order = client.create_order(
-        symbol=PAIR,
-        side=SIDE_BUY,
-        type=ORDER_TYPE_MARKET,
-        quantity=quantity
-    )
-    return order
-
-def buyLimit(quantity, price):
-    order = client.create_order(
-        symbol=PAIR,
-        side=SIDE_BUY,
-        type=ORDER_TYPE_LIMIT,
-        timeInForce=TIME_IN_FORCE_GTC,
-        quantity=quantity,
-        price=price
-    )
-    return order
-
-def sellLimit(quantity, price):
-    order = client.create_order(
-        symbol=PAIR,
-        side=SIDE_SELL,
-        type=ORDER_TYPE_LIMIT,
-        timeInForce=TIME_IN_FORCE_GTC,
-        quantity=quantity,
-        price=price
-    )
-    return order
-
-def calculateOrderProfit(order):
-    if order['side'] == 'BUY':
-        buyPrice = float(order['price'])
-        sellPrice = float(getPrice())
-    else:
-        sellPrice = float(order['price'])
-        buyPrice = float(getPrice())
-    return ((sellPrice - buyPrice) / buyPrice) * 100
-
-def getOrders():
-    orders = client.get_all_orders(symbol=PAIR)
-    return orders
-
 def getBalance():
     balance = client.get_asset_balance(asset='EUR')
     return balance
 
-def calculateLastWeekHighLow():
-    klines = client.get_historical_klines(PAIR, Client.KLINE_INTERVAL_1WEEK, "1 week ago UTC")
-    lastWeekHigh = 0
-    lastWeekLow = 999999999
-    for kline in klines:
-        lastWeekHigh = max(lastWeekHigh, float(kline[2]))
-        lastWeekLow = min(lastWeekLow, float(kline[3]))
-    return lastWeekHigh, lastWeekLow
+def calculateHighLow():
+    klines = client.get_historical_klines(PAIR, "1w", "1 week ago UTC")
+    lastWeekHigh = klines[-1][2]
+    lastWeekLow = klines[-1][3]
+    return float(lastWeekHigh), float(lastWeekLow)
 
-def calculateLastMonthHighLow():
-    klines = client.get_historical_klines(PAIR, Client.KLINE_INTERVAL_1MONTH, "1 month ago UTC")
-    lastMonthHigh = 0
-    lastMonthLow = 999999999
-    for kline in klines:
-        lastMonthHigh = max(lastMonthHigh, float(kline[2]))
-        lastMonthLow = min(lastMonthLow, float(kline[3]))
-    return lastMonthHigh, lastMonthLow
+waiting = False
+while True:
+    # Calulate 1/4 of the ranfe between the high and low of the last week
+    lastWeekHigh, lastWeekLow = calculateHighLow()
+    currentPrice = float(getPrice())
+    lastWeekRange = lastWeekHigh - lastWeekLow
+    quarterRange = lastWeekRange / 4
+    entry = (lastWeekLow + quarterRange)
+    percentRange = 1 + (quarterRange / entry)
+    print("percentRange:",percentRange)
+    print("Entry:",entry)
+    print("Current:",currentPrice)
+    print("Waiting:",waiting)
+    print()
 
-def calculateHighLow(interval, time):
-    klines = client.get_historical_klines(PAIR, interval, time)
-    high = 0
-    low = 999999999
-    for kline in klines:
-        high = max(high, float(kline[2]))
-        low = min(low, float(kline[3]))
-    return high, low
+    if(currentPrice < entry and waiting == False):
+        print("Buying")
+        order = client.create_test_order(
+            symbol=PAIR,
+            side=SIDE_BUY,
+            type=ORDER_TYPE_MARKET,
+            quantity=QUANTITY,  # Replace with the desired quantity to buy
+        )
 
-def percentageChange(price1, price2):
-    return ((price2 - price1) / price1) * 100
+        take_profit_price = float(order['price']) * TAKEPROFIT  # Set take profit price as 1% above the buy price
 
-print(getPrice())
+        client.create_test_order(
+            symbol=PAIR,
+            side=SIDE_SELL,
+            type=ORDER_TYPE_LIMIT,
+            quantity=QUANTITY,  # Replace with the desired quantity to sell
+            price=take_profit_price,
+        )
+        print("Bought at:",order['price'])
 
-# Calulate 1/4 of the ranfe between the high and low of the last week
-lastWeekHigh, lastWeekLow = calculateLastMonthHighLow()
-print(lastWeekHigh, lastWeekLow)
-lastWeekRange = lastWeekHigh - lastWeekLow
-print(lastWeekRange)
-quarterRange = lastWeekRange / 4
-buyLimitPrice = (lastWeekLow + quarterRange)
-print(buyLimitPrice)
-sellLimitPrice = (lastWeekHigh - quarterRange)
-print(sellLimitPrice)
-print(percentageChange(buyLimitPrice, sellLimitPrice))
+        stop_loss_price = float(order['price']) * STOPLOSS  # Set stop loss price as 1% below the buy price
+
+        client.create_test_order(
+            symbol=PAIR,
+            side=SIDE_SELL,
+            type=ORDER_TYPE_STOP_LOSS_LIMIT,
+            quantity=QUANTITY,  # Replace with the desired quantity to sell
+            stopPrice=stop_loss_price,
+            price=stop_loss_price,
+        )
+
+        waiting = True
+
+    if(currentPrice > entry + quarterRange and waiting == True):
+        waiting = False
+
+    sleep(1)
 
--- test.py
+++ test.py
@@ -2,8 +2,8 @@ from binance.client import Client
 from account import Account
 
 # Constants
-PAIR = "EURUSDT"
-QUANTITY = 1
+PAIR = "BTCEUR"
+QUANTITY = 0.001
 STOPLOSS = 0
 TAKEPROFIT = 1
 
@@ -13,14 +13,37 @@ account = Account(1000)
 account.verbose = False
 
 # Fetch
-candles = client.get_historical_klines(PAIR,'1m','1 month ago UTC')
+candles = client.get_historical_klines(PAIR,'1m', "21 Feb, 2024", "26 Feb, 2024")
+
+candlesCalculate = client.get_historical_klines(PAIR,'1d', "16 Feb, 2024", "21 Feb, 2024")
+high = 0
+low = 1000000
+for candle in candlesCalculate:
+    if float(candle[2]) > high:
+        high = float(candle[2])
+    if float(candle[3]) < low:
+        low = float(candle[3])
+quarter = (high - low) / 4
+entry = low + quarter
 
 for candle in candles:
     #print(candle[2]) # High
     #print(candle[3]) # Low
     #print()
 
-    account.buy(QUANTITY, candle[4], TAKEPROFIT, STOPLOSS)
+    if float(candle[2]) <= entry:
+        account.buy(QUANTITY, candle[4], TAKEPROFIT, STOPLOSS)
+
+    if float(candle[2]) < low:
+        low = float(candle[2])
+        quarter = (high - low) / 4
+        entry = low + quarter
+    
+    if float(candle[3]) > high:
+        high = float(candle[3])
+        quarter = (high - low) / 4
+        entry = low + quarter
+
     account.check_tp_sl(candle[2], candle[3])
 
 
